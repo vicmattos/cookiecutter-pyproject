9a37bd1c415beed1053b85bbda0e5ad2954b6ac9
Victor Mattos <5757883+vicmattos@users.noreply.github.com>
2023-11-22 19:42:16 -0300

    Add `tests/test_bake.py`

---
+++ tests/test_bake.py
@@ -0,0 +1,29 @@
+from contextlib import contextmanager
+from cookiecutter.utils import rmtree
+
+@contextmanager
+def bake_in_temp_dir(cookies, *args, **kwargs):
+    """
+    Delete the temporal directory that is created when executing the tests
+    :param cookies: pytest_cookies.Cookies,
+        cookie to be baked and its temporal files will be removed
+    """
+    result = cookies.bake(*args, **kwargs)
+    try:
+        yield result
+    finally:
+        rmtree(str(result.project_path))
+
+
+def test_bake_with_defaults(cookies):
+    with bake_in_temp_dir(cookies) as result:
+        assert result.project_path.is_dir()
+        assert result.exit_code == 0
+        assert result.exception is None
+
+        # breakpoint()
+        found_toplevel_files = [f.name for f in result.project_path.iterdir()]
+        assert '.editorconfig' in found_toplevel_files
+        assert '.gitignore' in found_toplevel_files
+        assert 'Makefile' in found_toplevel_files
+        assert 'pyproject.toml' in found_toplevel_files
