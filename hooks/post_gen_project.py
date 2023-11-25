e8af6bd2e8becf7ed05373fe1025c666748de548
Victor Mattos <5757883+vicmattos@users.noreply.github.com>
2023-11-25 14:57:34 -0300

    Refactor `post_gen_project.py` to isolate `pyproject.toml` handle

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,7 +1,18 @@
+from __future__ import annotations
 from pathlib import Path
 from shutil import rmtree
 import tomli
 import tomli_w
+
+
+def toml_handler(filepath: Path) -> tuple[dict, callable]:
+    with open(filepath, 'rb') as f:
+        config = tomli.load(f)
+    def writer(config):
+        with open(filepath, 'wb') as f:
+            tomli_w.dump(pytoml, f)
+    return config, writer
+
 
 
 if __name__ == "__main__":
@@ -10,10 +21,7 @@ if __name__ == "__main__":
         Path("LICENSE").unlink()
 
     if '{{cookiecutter.documentation_framework}}' == 'No documentation':
-        Path("mkdocs.yml").unlink()
         rmtree("docs/")
-        with open('pyproject.toml', 'rb') as f:
-            pytoml = tomli.load(f)
-        pytoml['project']['optional-dependencies']['dev'].remove("mkdocs-material")
-        with open('pyproject.toml', 'wb') as f:
-            tomli_w.dump(pytoml, f)
+        Path("mkdocs.yml").unlink()
+        pytoml, writer = toml_handler(Path('pyproject.toml'))
+        writer(pytoml['project']['optional-dependencies']['dev'].remove("mkdocs-material"))
