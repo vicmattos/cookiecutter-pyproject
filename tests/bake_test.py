e7ace766b5df601354ea28d8c048af635fa49bb2
Victor Mattos <5757883+vicmattos@users.noreply.github.com>
2023-11-27 14:35:51 -0300

    ci(template): add `ruff` as `pre-commit` hook

--- tests/bake_test.py
+++ tests/bake_test.py
@@ -24,11 +24,11 @@ def test_bake_and_run_make_setup(cookies, bake_in_temp_dir, run_inside_dir):
         assert run_inside_dir('make setup', result.project_path) == 0
 
 
-@pytest.mark.parametrize("pyversion", ['3.11', '3.10', '3.9', '3.8'])
+@pytest.mark.parametrize('pyversion', ['3.11', '3.10', '3.9', '3.8'])
 def test_bake_every_python_version(cookies, bake_in_temp_dir, pyversion):
     with bake_in_temp_dir(
         cookies,
-        extra_context={'python_version': pyversion}
+        extra_context={'python_version': pyversion},
     ) as result:
         root: Path = result.project_path
         assert f'requires-python = ">={pyversion}' in (root / 'pyproject.toml').read_text()
@@ -38,7 +38,7 @@ def test_bake_every_python_version(cookies, bake_in_temp_dir, pyversion):
 def test_bake_not_open_source(cookies, bake_in_temp_dir):
     with bake_in_temp_dir(
         cookies,
-        extra_context={'open_source_license': 'Not open source'}
+        extra_context={'open_source_license': 'Not open source'},
     ) as result:
         found_toplevel_files = [f.name for f in result.project_path.iterdir()]
         assert 'LICENSE' not in found_toplevel_files
@@ -54,7 +54,7 @@ def test_bake_and_run_make_docs_build(cookies, bake_in_temp_dir, run_inside_dir)
 def test_bake_no_docs_framework(cookies, bake_in_temp_dir, text_in_file):
     with bake_in_temp_dir(
         cookies,
-        extra_context={'documentation_framework': 'No documentation'}
+        extra_context={'documentation_framework': 'No documentation'},
     ) as result:
         found_toplevel_files = [f.name for f in result.project_path.iterdir()]
         assert 'mkdocs.yml' not in found_toplevel_files
@@ -86,3 +86,10 @@ def test_bake_with_callable_cli(cookies, bake_in_temp_dir, run_inside_dir):
         extra_context={'project_slug': 'pkg_command'},
     ) as result:
         assert run_inside_dir('python src/pkg_command.py --help', result.project_path) == 0
+
+
+@pytest.mark.slow
+def test_run_pre_commit(cookies, bake_in_temp_dir, run_inside_dir):
+    with bake_in_temp_dir(cookies) as result:
+        run_inside_dir('git init .', result.project_path)
+        assert run_inside_dir('make pre-commit-run', result.project_path) == 0
