5098eea6e3ca7248f693debb2f6f7e10c80f7f09
Victor Mattos <5757883+vicmattos@users.noreply.github.com>
2023-11-28 10:58:04 -0300

    feat(template): add `command_line_interface` option with `typer`

--- tests/bake_test.py
+++ tests/bake_test.py
@@ -71,19 +71,27 @@ def test_bake_no_docs_framework(cookies, bake_in_temp_dir, text_in_file):
 
 
 @pytest.mark.slow
-def test_bake_with_cli_package(cookies, bake_in_temp_dir, run_inside_dir):
+@pytest.mark.parametrize('cli_tool', ['Typer', 'Argparse'])
+def test_bake_with_cli_package(cookies, bake_in_temp_dir, cli_tool, run_inside_dir):
     with bake_in_temp_dir(
         cookies,
-        extra_context={'project_folder': 'pkg_command'},
+        extra_context={
+            'project_folder': 'pkg_command',
+            'command_line_interface': cli_tool,
+        },
     ) as result:
         assert run_inside_dir('make install-editable', result.project_path) == 0
         assert run_inside_dir('.venv/bin/pkg_command --help', result.project_path) == 0
 
 
-def test_bake_with_callable_cli(cookies, bake_in_temp_dir, run_inside_dir):
+@pytest.mark.parametrize('cli_tool', ['Typer', 'Argparse'])
+def test_bake_with_callable_cli(cookies, bake_in_temp_dir, cli_tool, run_inside_dir):
     with bake_in_temp_dir(
         cookies,
-        extra_context={'project_slug': 'pkg_command'},
+        extra_context={
+            'project_slug': 'pkg_command',
+            'command_line_interface': cli_tool,
+        },
     ) as result:
         assert run_inside_dir('python src/pkg_command.py --help', result.project_path) == 0
 
@@ -93,3 +101,25 @@ def test_run_pre_commit(cookies, bake_in_temp_dir, run_inside_dir):
     with bake_in_temp_dir(cookies) as result:
         run_inside_dir('git init .', result.project_path)
         assert run_inside_dir('make pre-commit-run', result.project_path) == 0
+
+
+@pytest.mark.parametrize(
+    ('cli_tool', 'is_not_present'),
+    [
+        ('Typer', False),
+        ('Argparse', True),
+        ('No command-line interface', True),
+    ]
+)
+def test_bake_cli_typer_as_dependency(cookies, bake_in_temp_dir, text_in_file, cli_tool, is_not_present):
+    with bake_in_temp_dir(
+        cookies,
+        extra_context={
+            'project_slug': 'project_slug',
+            'command_line_interface': cli_tool
+        }
+    ) as result:
+        pyproject_toml = result.project_path / 'pyproject.toml'
+        assert text_in_file('typer', pyproject_toml, negate=is_not_present)
+        source_code = result.project_path / 'src' / 'project_slug.py'
+        assert text_in_file('typer', source_code, negate=is_not_present)
--- {{cookiecutter.project_folder}}/src/{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_folder}}/src/{{cookiecutter.project_slug}}.py
@@ -1,3 +1,14 @@
+{%- if cookiecutter.command_line_interface|lower == 'typer' %}
+import typer
+
+
+def main(verbose: bool = False):
+    if verbose:
+        typer.echo(f"Arguments: {locals().keys()}")
+    typer.echo("Replace this message by putting your code into src/{{cookiecutter.project_slug}}.py")
+
+
+{%- elif cookiecutter.command_line_interface|lower == 'argparse' %}
 import argparse
 from collections.abc import Sequence
 
@@ -17,5 +28,12 @@ def cli(argv: Sequence[str] | None = None) -> int:
     return 0
 
 
+{%- endif %}
+
+
 if __name__ == '__main__':
+    {%- if cookiecutter.command_line_interface|lower == 'typer' %}
+    typer.run(main)
+    {%- elif cookiecutter.command_line_interface|lower == 'argparse' %}
     raise SystemExit(cli())
+    {%- endif %}
